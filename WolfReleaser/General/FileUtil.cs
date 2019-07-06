9da9007d6c2f6668b4b17cc3ac82627c27a2965a
Sipi Ovaska <sipi@live.fi>
2019-07-06 20:36:19 +0300

    Add temporary files helper class

---
+++ WolfReleaser/General/FileUtil.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfReleaser.General
+{
+    public static class FileUtil
+    {
+        public static string AppDataFolder
+        {
+            get
+            {
+                var appdata = Environment.GetFolderPath(
+                    Environment.SpecialFolder.ApplicationData);
+                var path = Path.Combine(appdata, "Pack3r");
+
+                if (!Directory.Exists(path))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(path);
+                        Log.Debug($"Created working directory '{path}'");
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Fatal($"Failed to create working directory " +
+                            $"to '{path}': {e.Message}");
+                        Log.Debug(e.ToString());
+                        return null;
+                    }
+                }
+
+                return path;
+            }
+        }
+
+        public static string NewTempFolder
+        {
+            get
+            {
+                var path = Path.Combine(AppDataFolder, Guid.NewGuid().ToString());
+
+                try
+                {
+                    Directory.CreateDirectory(path);
+                    Log.Debug($"Created temporary directory '{path}'");
+                }
+                catch (Exception e)
+                {
+                    Log.Fatal($"Failed to create temporary directory " +
+                        $"to '{path}': {e.Message}");
+                    Log.Debug(e.ToString());
+                    return null;
+                }
+
+                return path;
+            }
+        }
+
+        public static void DeleteTempData()
+        {
+            foreach (var path in Directory.GetDirectories(AppDataFolder))
+            {
+                try
+                {
+                    Directory.Delete(path);
+                    Log.Debug($"Deleted temporary directory '{path}'");
+                }
+                catch (Exception e)
+                {
+                    Log.Error($"Failed to delete temporary directory " +
+                        $"to '{path}': {e.Message}");
+                    Log.Debug(e.ToString());
+                }
+            }
+        }
+    }
+}
--- WolfReleaser/MainWindow.xaml.cs
+++ WolfReleaser/MainWindow.xaml.cs
@@ -5,7 +5,10 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,11 +26,24 @@ using WolfReleaser.Parsers;
 
 namespace WolfReleaser
 {
+    public enum UIState
+    {
+        NeedMap = 0,
+        MissingFiles,
+        ReadyToPack,
+        Packed,
+        FatalError = int.MaxValue
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private bool forceClose = false;
+
+        #region UI Bind Properties
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private LogLevel _loglevel = LogLevel.Fatal;
@@ -39,41 +55,58 @@ namespace WolfReleaser
                 if (value != _loglevel)
                 {
                     _loglevel = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LogLevel)));
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private UIState _uistate = UIState.NeedMap;
+        public UIState UIState
+        {
+            get => _uistate;
+            set
+            {
+                if (value != _uistate)
+                {
+                    _uistate = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(StateNeedMap));
+                    OnPropertyChanged(nameof(StateMissingFiles));
+                    OnPropertyChanged(nameof(StateReadyToPack));
+                    OnPropertyChanged(nameof(StatePacked));
+                    OnPropertyChanged(nameof(StateFatalError));
                 }
             }
         }
 
         public ObservableCollection<LogEntry> LogLines { get; }
 
+        public bool StateNeedMap => _uistate == UIState.NeedMap;
+        public bool StateMissingFiles => _uistate == UIState.MissingFiles;
+        public bool StateReadyToPack => _uistate == UIState.ReadyToPack;
+        public bool StatePacked => _uistate == UIState.Packed;
+        public bool StateFatalError => _uistate == UIState.FatalError;
+
+        #endregion UI Bind Properties
+
         public MainWindow()
         {
             this.LogLines = new ObservableCollection<LogEntry>();
-            this.LogLines.CollectionChanged += delegate (object s, NotifyCollectionChangedEventArgs _)
-            {
-                this.ConsoleScrollViewer.ScrollToEnd();
-            };
+            this.LogLines.CollectionChanged +=
+                delegate (object s, NotifyCollectionChangedEventArgs _)
+                {
+                    this.ConsoleScrollViewer.ScrollToEnd();
+                };
 
             InitializeComponent();
             this.DataContext = this;
 
             this.InitConsole();
 
-            var fc = new MapFileCollection(@"C:\Temp\ET\map\ET\etmain\maps\inertia.map");
-
-            var allFilez = fc.GetFiles();
-
-            var sw = Stopwatch.StartNew();
-
-            foreach (var f in allFilez)
-            {
-                Console.WriteLine($"{f.target.PadRight(40)} in '{f.source}'");
-            }
-            //var mapParser = new MapParser(@"C:\Temp\ET\map\ET\etmain\maps\railgun_final.map");
-            //var map = mapParser.Parse();
-            //var shaders = ShaderParser.ReadShaders(@"C:\Temp\ET\map\ET\etmain\scripts").ToList();
-            //var requiredFiles = ShaderParser.GetRequiredFiles(map, shaders);
-            sw.Stop();
+            var ass = Assembly.GetExecutingAssembly().GetName().Version;
+            Log.Info($"Pack3r Version {ass}");
+            Log.Info($"Report issues at github.com/sibel1us/Pack3r");
+            Log.Info($"");
         }
 
 
@@ -163,6 +196,56 @@ namespace WolfReleaser
             }
 
             Clipboard.SetText(sb.ToString());
+        }
+
+        protected void OnPropertyChanged(
+            [CallerMemberName]string propertyName = null)
+        {
+            if (propertyName != null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((int)this.LogLevel > (int)LogLevel.Info)
+                this.LogLevel = LogLevel.Info;
+
+            var help = new[]
+            {
+                "Select the .map file you wish to analyze, and select the output " +
+                "folder to write packed pk3 to.",
+                "You can choose to include the .map source file, but you do not need to.",
+                "Note that files that are unneeded in the release are NOT included. You will " +
+                "have to include them yourself. This includes (but might not be limited to): " +
+                "editorimages, lightimages, misc_models",
+                "You can also choose a different release name for your map, so you don't have to " +
+                "keep renaming mapscripts and such to different beta versions.",
+                "",
+                "Note: this tool makes multiple assumptions about your folder structure:",
+                "- You have everything in their normal locations, meaning the .map file " +
+                "is in etmain/maps, and that other required files (mapscripts and such) are " +
+                "in their correct folders. Put log-level to 'All' to see information about " +
+                "possible missing files.",
+                "- The program needs to have write access to %appdata% and the folder where " +
+                "the release pk3 is packed to.",
+                "",
+            };
+
+            foreach (var h in help)
+                Log.Info(h);
+
+            Log.Info("");
+        }
+
+        private void MainWnd_Closing(object sender, CancelEventArgs e)
+        {
+            if (!forceClose)
+            {
+                Log.Info("Cleaning up temporary files...");
+                FileUtil.DeleteTempData();
+            }
         }
     }
 
